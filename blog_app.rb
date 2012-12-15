cce809a6716e0e9281f2b4ddd19c793967199cba
carlitos-esquer <carlitos.esquer@gmail.com>
2012-12-14 23:16:42 -0700

    Agregado de articulos

--- blog_app.rb
+++ blog_app.rb
@@ -37,6 +37,7 @@ get '/:id/blogs' do
       usuario = b[params[:id]]
     end
     return slim :no_user if usuario.nil?
+    @id_usuario = params[:id]
     @blogs = usuario.blogs
     return slim :no_blog if @blogs.nil? or @blogs.empty?
     slim :blogs
@@ -91,3 +92,32 @@ get '/:id/:blog' do
     slim :articulos
 end
 
+get '/:id/:blog/articulos/new' do
+	slim :articulo_new
+end
+#Revisar el metodo de guardar Articulos (Temas o Posts)
+post '/:id/:blog/articulos/new' do
+    usuario = nil
+    blogs.transaction do |b|
+      usuario = b[params[:id]]
+    end
+    @titulo = ""
+    @articulos = ""
+    return slim :no_user if usuario.nil?
+    blog = usuario.blog(params[:blog])
+    return slim :no_blog if blog.nil?
+    @titulo = blog.titulo
+    articulo = Articulo.new 1000, params[:titulo]
+    articulo.contenido = params[:contenido]
+    blog.articulos = [] if blog.articulos.nil?
+	blog.articulos << articulo
+    index = 0
+    usuario.blogs.each do |b|
+		usuario.blogs[index] = blog if  usuario.blogs[index].id == blog.id
+		index += 1
+    end
+    blogs.transaction do |b|
+      b[usuario.cuenta] = usuario
+    end
+    redirect "/#{params[:id]}/#{params[:blog]}"
+end
